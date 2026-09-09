9f8504f8e47b54440da96f3b9d53ad765a8f89c2
kimiuus <kimi.uusi.rantala@gmail.com>
2026-09-09 16:01:20 +0300

    pizza update

--- Projects/Python/Assignment 7/7.6 pizza.py
+++ Projects/Python/Assignment 7/7.6 pizza.py
@@ -1,15 +1,17 @@
 from math import pi
 def pizza(num, hinta):
-    h = num / 100
+    m = num / 100
+    h = m / 2
     p = pi*(h*h)
     c = hinta / p
-    return c
-num = int(input("Syötä halkaisija: "))
-hinta = int(input("Syötä hinta: "))
+    f = round(c, 2)
+    return f
+num = float(input("Syötä halkaisija: "))
+hinta = float(input("Syötä hinta: "))
 pizza1 = pizza(num, hinta)
-num = int(input("Syötä seuraava halkaisija: "))
-hinta = int(input("Syötä seuraava hinta: "))
+num = float(input("Syötä seuraava halkaisija: "))
+hinta = float(input("Syötä seuraava hinta: "))
 pizza2 = pizza(num, hinta)
 if pizza1 > pizza2:
-    print(f"Pizza 2 {pizza2:.3}€/neliömetri on halvempi kun pizza 1 {pizza1:.3}€/neliömetri")
-else:print(f"Pizza 1 {pizza1:.3}€/neliömetri on halvempi kun pizza 2 {pizza2:.3}€/neliömetri")
+    print(f"Pizza 2 {pizza2}€/neliömetri on halvempi kun pizza 1 {pizza1}€/neliömetri")
+else:print(f"Pizza 1 {pizza1}€/neliömetri on halvempi kun pizza 2 {pizza2}€/neliömetri")
