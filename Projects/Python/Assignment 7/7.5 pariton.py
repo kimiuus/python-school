3d535aff66befe6c99fad0c288a902f1a3e85512
kimiuus <kimi.uusi.rantala@gmail.com>
2026-09-18 14:27:36 +0300

    18.9 update 2

--- Projects/Python/Assignment 7/7.5 pariton.py
+++ Projects/Python/Assignment 7/7.5 pariton.py
@@ -1,14 +1,17 @@
-lista = []
-num = 0
 def jako(lista):
     for num in (lista):
         if num % 2 == 0:
-            print(num)
+            lista2.append(num)
+    return lista2
+lista = []
+lista2 = []
+num = 0
 while num != "":
     try:
         num = int(input("Syötä seuraava kokonaisluku: "))
         lista.append(num)
     except ValueError:
         break
+uusi = jako(lista)
 print(lista)
-jako(lista)
+print(uusi)
